61a3ec6c5460cf1af8b1016f4318b75c8629a077
GustavoRodak-Pormade <guthogamer041@gmail.com>
2026-04-22 17:29:22 -0300

    Update CursoClicado.tsx

--- FrontEnd/src/pages/CursoClicado/CursoClicado.tsx
+++ FrontEnd/src/pages/CursoClicado/CursoClicado.tsx
@@ -1,6 +1,14 @@
+import Barra from "../../components/Barra/Barra";
+
 export default function CursoClicado(){
   return(
     <>
+      <div>
+        <Barra/>
+        <div>
+          
+        </div>
+      </div>
     </>
   );
 }
